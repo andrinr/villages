c3c95a1fb061643e673b87d9c3c13ffeb73a8467
Andrin Rehmann <andrinrehmann@gmail.com>
2023-04-30 17:12:13 +0200

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Villages
 
+Naviage, explore and discover this virtual village.
+
+![Villages](villages.png)
+
+The project uses [Svelte](https://svelte.dev/), [Three.js](https://threejs.org/) and [Typescript](https://www.typescriptlang.org/). It features a small animation system, a simple state management system and a simple event system.
+
+
 ## Get started
 
 1. ``npm install``
--- src/animation/loader.ts
+++ src/animation/loader.ts
@@ -5,7 +5,9 @@ import type THREE from 'three';
 const loadGLTF = async (modelPath, scene : THREE.Scene) => {
     const dracoLoader = new DRACOLoader();
     dracoLoader.setDecoderConfig({ type: 'js' });
-    dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+    dracoLoader.setDecoderPath( 'models/draco/' );
+
+    //dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
     const gltfLoader = new GLTFLoader();
     gltfLoader.setDRACOLoader(dracoLoader);
         
